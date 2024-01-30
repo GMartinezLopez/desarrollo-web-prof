97e573ed1195e30803c437f3760027c33967ea89
Gael Ulises Martínez López <gaelml22@gmail.com>
2024-01-30 14:47:07 -0500

    Update HomeView.vue
    Corrección de validaciones en los 2 inputs

--- Directivas/src/views/HomeView.vue
+++ Directivas/src/views/HomeView.vue
@@ -1,9 +1,53 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import {ref} from 'vue';
+import type {Ref} from 'vue';
+
+const name:Ref<string> = ref ('Ricardo Anaya');
+const age:Ref<number> = ref (21);
+const errors = ref([{
+  name: '',
+  age: ''
+}]);
+
+const error:Ref<string> = ref ('#d03');
+
+const validation = () => {
+  if(name.value.length < 5) {
+    errors.value.push({name:'Error de longitud', age:''});
+  }
+}
+
+const validation2 = () => {
+  if(age.value < 5) {
+    errors.value.push({name:'', age:'Error de edad'});
+  }
+}
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div>
+      <input @input="validation()" v-model="name" type="text">
+    </div>
+    <div>
+      <input @input="validation2()" v-model="age" type="text">
+    </div>
+
+    <h1 v-bind:style="{'color':error, 'background-color':'#cd3'}">{{ name }}</h1>
+    <h2 :class="{'error': age > 10}">{{ age }}</h2>
+
+    <h3>Errors:</h3>
+    <span v-for="(err, index) in errors" :key="index">      
+        {{ err }}
+    </span>
   </main>
 </template>
+
+<style scoped>
+.error{
+  color: white;
+  background-color: red;
+  border: 1px solid white;
+}
+</style>
