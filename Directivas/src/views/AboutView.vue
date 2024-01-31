f9b0585e2bbf264876d35c6ef39453da83c0e7bf
Gael Ulises Martínez López <gaelml22@gmail.com>
2024-01-31 16:53:06 -0500

    correcciones en Formulario

--- Directivas/src/views/AboutView.vue
+++ Directivas/src/views/AboutView.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import {ref} from 'vue';
-import type {Ref} from 'vue';
+import { ref } from 'vue';
+import type { Ref } from 'vue';
 
 const selectedOption: Ref<string> = ref('1'); // Valor predeterminado
-const name:Ref<string> = ref ('Ricardo');
-const apellido:Ref<string> = ref ('Anaya');
-const age:Ref<number> = ref (21);
-const genero:Ref<string> = ref ('Helicóptero apache');
+const name: Ref<string> = ref('Ricardo');
+const apellido: Ref<string> = ref('Anaya');
+const age: Ref<number> = ref(21);
+const genero: Ref<string> = ref('Helicóptero apache');
 const errors = ref([{
   name: '',
   apellido: '',
@@ -14,26 +14,26 @@ const errors = ref([{
   genero: '',
 }]);
 
-const error:Ref<string> = ref ('#d03');
+const error: Ref<string> = ref('#d03');
 
 const validation = () => {
 
   errors.value = [];
 
-  if(name.value.length < 5 || name.value.length > 18) {
-    errors.value.push({name:'Error de longitud', apellido:'', age:'', genero:''});
+  if (name.value.length < 5 || name.value.length > 18) {
+    errors.value.push({ name: 'Error de longitud', apellido: '', age: '', genero: '' });
   }
 
-  if(apellido.value == name.value) {
-    errors.value.push({name:'', apellido:'Error: el apellido no puede ser igual al nombre.', age:'', genero:''});
+  if (apellido.value == name.value) {
+    errors.value.push({ name: '', apellido: 'Error: el apellido no puede ser igual al nombre.', age: '', genero: '' });
   }
 
-  if(age.value < 0 || age.value > 60) {
-    errors.value.push({name:'', apellido:'', age:'Error, la edad debe estar entre 0 y 60 años.', genero:''});
+  if (age.value < 0 || age.value > 60) {
+    errors.value.push({ name: '', apellido: '', age: 'Error, la edad debe estar entre 0 y 60 años.', genero: '' });
   }
 
-  if(age.value < 0 || age.value > 60) {
-    errors.value.push({name:'', apellido:'', age:'Error, la edad debe estar entre 0 y 60 años.', genero:''});
+  if (genero.value != 'Helicóptero apache') {
+    errors.value.push({ name: '', apellido: '', age: '', genero: 'Error, solo puedes ser un helicóptero apache' });
   }
 }
 
@@ -44,41 +44,41 @@ const validation = () => {
 <template>
   <main>
     <div class="about">
-    <h1>Actividad: Fromulario</h1>
-  </div>
-  <div @input="validation()">
-    <div>
-      <input v-model="name" type="text">
+      <h1>Actividad: Fromulario</h1>
     </div>
-    <div>
-      <input v-model="apellido" type="text">
-    </div>
-    <div>
-      <input v-model="age" type="text">
-    </div>   
-    <div>
+    <div @input="validation()">
       <div>
-        <select v-model="selectedOption">
-			<option value="1">Masculino</option>
-			<option value="2">Femenino</option>
-			<option value="3">Otro</option>
-	</select>
+        <input v-model="name" type="text">
       </div>
       <div>
-        <input type="text" :disabled="selectedOption == '1'">
+        <input v-model="apellido" type="text">
       </div>
-    </div>
+      <div>
+        <input v-model="age" type="text">
+      </div>
+      <div>
+        <div>
+          <select v-model="selectedOption">
+            <option value="1">Masculino</option>
+            <option value="2">Femenino</option>
+            <option value="3">Otro</option>
+          </select>
+        </div>
+        <div>
+          <input v-model="genero" type="text" :disabled="selectedOption != '3'">
+        </div>
+      </div>
 
-    <h3>Errors:</h3>
-    <span v-for="(err, index) in errors" :key="index">      
+      <h3>Errors:</h3>
+      <span v-for="(err, index) in errors" :key="index">
         {{ err }}
-    </span>
-  </div>
+      </span>
+    </div>
   </main>
 </template>
 
 <style scoped>
-.error{
+.error {
   color: white;
   background-color: red;
   border: 1px solid white;
