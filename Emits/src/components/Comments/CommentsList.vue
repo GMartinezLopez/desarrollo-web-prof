ab3ce598abe3a6b9c45fddeb5c4cdc8e947523f1
Gael Ulises Martínez López <54154672+GMartinezLopez@users.noreply.github.com>
2024-02-15 17:47:55 -0500

    Clase: Datos Computados

--- Emits/src/components/Comments/CommentsList.vue
+++ Emits/src/components/Comments/CommentsList.vue
@@ -2,22 +2,95 @@
   <div>
     <button @click="clicked($event)">CLICKEAME</button>
     <ul>
-      <CommentItem @say-good-bye="clicked($event)" v-bind="comments" v-for="(comments, index) in CommentsData" :key="index" />
+      <CommentItem @say-good-bye="clicked($event)" v-bind="comments" v-for="(comments, index) in CommentsData"
+        :key="index" />
     </ul>
+
+    <input type="number" v-model="validation" />
+    <span v-if="validation">{{ age }}</span>
+
+    <input type="number" v-model="dolars" />
+    {{ dolars }}
+    {{ total }}
   </div>
 </template>
 
 <script setup lang="ts">
 import CommentsData from '@/data/CommentsData'
 import CommentItem from './CommentItem.vue'
+import { ref, computed } from 'vue';
+//import { onBeforeMount, onMounted } from 'vue';
 
 const clicked = (event: any) => {
   console.log(event);
-  
-    alert('Seleccionaste: ' + event);
 }
+const name = ref('')
+const age = ref(0)
+const dolars = ref(0)
+
+const validation = computed(() => age.value > 18)
+const total = computed(() => dolars.value * 2)
+
+// const validation = () => {
+//   if(age.value > 0){
+//     console.log('si')
+//   }else
+//   console.log('no')
+// }
+
+// onMounted(() => {
+//   console.log("despues de ...")
+// })
+// onBeforeMount(async () => {
+//   const data = await fetch("/url");
+// })
 
 </script>
+
+<!-- <script lang="ts">
+import { defineComponent } from 'vue';
+import CommentsData from '@/data/CommentsData'
+import CommentItem from './CommentItem.vue'
+
+export default defineComponent({
+
+  name: 'CommentsList',
+
+  setup() { },
+
+  methods: {
+    clicked(event: any) {
+      console.log(event);
+    }
+  },
+  data() {
+    return {
+      CommentsData,
+      CommentItem
+    }
+  },
+
+  components: CommentItem,
+
+  onBeForeMount() {
+
+  },
+
+  onMounted() {
+
+  },
+
+  beforeCreate() {
+
+  },
+
+  created() {
+
+  },
+
+})
+
+</script> -->
 <style scoped></style>
 
 <!-- 
