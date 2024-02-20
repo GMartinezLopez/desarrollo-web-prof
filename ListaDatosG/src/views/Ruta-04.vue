2eecd6394c4cf0b6143dbf08855d9ceb822ecc7a
Gael Ulises Martínez López <54154672+GMartinezLopez@users.noreply.github.com>
2024-02-20 17:02:45 -0500

    Optimizacion de codigo en rutas

--- ListaDatosG/src/views/Ruta-04.vue
+++ ListaDatosG/src/views/Ruta-04.vue
@@ -1,20 +1,9 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
 import DataItem from '@/components/datos/DataItem.vue'
-import DataInfo01 from '@/data/DataInfo01'
-import DataInfo02 from '@/data/DataInfo02'
-import DataInfo03 from '@/data/DataInfo03'
 import DataInfo04 from '@/data/DataInfo04'
-import DataInfo05 from '@/data/DataInfo05'
-import DataInfo06 from '@/data/DataInfo06'
 
-
-const vistaActual01 = false;
-const vistaActual02 = false;
-const vistaActual03 = false;
 const vistaActual04 = true;
-const vistaActual05 = false;
-const vistaActual06 = false;
 </script>
 
 <template>
@@ -25,35 +14,10 @@ const vistaActual06 = false;
     <h1>Vista 4</h1>
   </header>
   <body>
-    <div class="">
-      <div v-if="vistaActual01">
-        <ul>
-          <DataItem v-bind="datos" v-for="(datos, index) in DataInfo01" :key="index" />
-        </ul>
-      </div>
-      <div v-else-if="vistaActual02">
-        <ul>
-          <DataItem v-bind="datos" v-for="(datos, index) in DataInfo02" :key="index" />
-        </ul>
-      </div>
-      <div v-else-if="vistaActual03">
-        <ul>
-          <DataItem v-bind="datos" v-for="(datos, index) in DataInfo03" :key="index" />
-        </ul>
-      </div>
-      <div v-else-if="vistaActual04">
+    <div>
+      <div v-if="vistaActual04">
         <ul>
           <DataItem v-bind="datos" v-for="(datos, index) in DataInfo04" :key="index" />
-        </ul>
-      </div>
-      <div v-else-if="vistaActual05">
-        <ul>
-          <DataItem v-bind="datos" v-for="(datos, index) in DataInfo05" :key="index" />
-        </ul>
-      </div>
-      <div v-else-if="vistaActual06">
-        <ul>
-          <DataItem v-bind="datos" v-for="(datos, index) in DataInfo06" :key="index" />
         </ul>
       </div>
     </div>
--- ListaDatosG/src/views/Ruta-06.vue
+++ ListaDatosG/src/views/Ruta-06.vue
@@ -1,19 +1,8 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
 import DataItem from '@/components/datos/DataItem.vue'
-import DataInfo01 from '@/data/DataInfo01'
-import DataInfo02 from '@/data/DataInfo02'
-import DataInfo03 from '@/data/DataInfo03'
-import DataInfo04 from '@/data/DataInfo04'
-import DataInfo05 from '@/data/DataInfo05'
 import DataInfo06 from '@/data/DataInfo06'
 
-
-const vistaActual01 = false;
-const vistaActual02 = false;
-const vistaActual03 = false;
-const vistaActual04 = false;
-const vistaActual05 = false;
 const vistaActual06 = true;
 </script>
 
@@ -25,33 +14,8 @@ const vistaActual06 = true;
     <h1>Vista 5</h1>
   </header>
   <body>
-    <div class="">
-      <div v-if="vistaActual01">
-        <ul>
-          <DataItem v-bind="datos" v-for="(datos, index) in DataInfo01" :key="index" />
-        </ul>
-      </div>
-      <div v-else-if="vistaActual02">
-        <ul>
-          <DataItem v-bind="datos" v-for="(datos, index) in DataInfo02" :key="index" />
-        </ul>
-      </div>
-      <div v-else-if="vistaActual03">
-        <ul>
-          <DataItem v-bind="datos" v-for="(datos, index) in DataInfo03" :key="index" />
-        </ul>
-      </div>
-      <div v-else-if="vistaActual04">
-        <ul>
-          <DataItem v-bind="datos" v-for="(datos, index) in DataInfo04" :key="index" />
-        </ul>
-      </div>
-      <div v-else-if="vistaActual05">
-        <ul>
-          <DataItem v-bind="datos" v-for="(datos, index) in DataInfo05" :key="index" />
-        </ul>
-      </div>
-      <div v-else-if="vistaActual06">
+    <div>
+      <div v-if="vistaActual06">
         <ul>
           <DataItem v-bind="datos" v-for="(datos, index) in DataInfo06" :key="index" />
         </ul>
